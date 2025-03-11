cd51a36d225a15ee760916750ca1c61e7717dfed
kvl-ballester <kvl.ballester@gmail.com>
2025-03-12 00:15:48 +0100

    header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import "./header.css"
+export default function Header() {
+
+    return (
+        <div className="header row">
+            <h2>AI NFT PROFILE PICTURE GENERATOR</h2>
+            <nav>
+                <ul>
+                    <li><a href="/gallery"><h3>GALLERY</h3></a></li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
